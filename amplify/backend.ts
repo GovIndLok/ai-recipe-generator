ef4d59ddb43a59de32d0c3efffd06894abc8b73f
GovIndLok <97396655+GovIndLok@users.noreply.github.com>
2026-01-16 21:54:39 +0530

    connect to bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,34 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  "https://bedrock-runtime.us-east-1.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: "ap-south-1",
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      "arn:aws:bedrock:ap-south-1::foundation-model/deepseek.v3-v1:0"
+    ],
+    actions: [
+      "bedrock:InvokeModel",
+    ]
+  })
+);
